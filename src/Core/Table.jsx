1ccd36fdffb63fc6bc4d66cae71da92adfa04ea8
taranpreet03 <taranpreet.kaur@omlogistics.co.in>
2026-01-08 11:53:02 +0530

    Body overflow fix done

--- src/Core/Table.jsx
+++ src/Core/Table.jsx
@@ -4,42 +4,49 @@ const Table = ({ columns = [], data = [] }) => {
   }
 
   return (
-    <div className="max-h-[95vh] overflow-y-auto text-[#3A4752]">
-      <table className="w-full border border-gray-200 text-m">
-       <thead className="sticky top-0 z-20 bg-[#DCE4FF]">
-  <tr>
-    {columns.map((col, index) => (
-      <th
-        key={index}
-        className="border-b border-[#F2F4F7] px-3 py-2 text-left font-normal bg-[#DCE4FF]"
-      >
-        {col.header}
-      </th>
-    ))}
-  </tr>
-</thead>
-
-
-        <tbody>
-          {data.map((row, rowIndex) => (
-            <tr
-              key={row._id || rowIndex}
-              className=""
-            >
-              {columns.map((col, colIndex) => (
-                <td
-                  key={colIndex}
-                  className={`border border-[#F2F4F7] px-3 py-2 ${
-                    col.header === "Action" ? "bg-white hover:bg-white" : ""
-                  }`}
-                >
-                  {col.render ? col.render(row, rowIndex) : row[col.accessor]}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
+    <div className="text-[#3A4752] border border-gray-200">
+      
+{/* table header */}
+      <table className="w-full border-collapse table-fixed">
+        <thead className="bg-[#DCE4FF]">
+          <tr>
+            {columns.map((col, index) => (
+              <th
+                key={index}
+                className="border-b border-[#F2F4F7] px-3 py-2 text-left font-normal bg-[#DCE4FF]"
+              >
+                {col.header}
+              </th>
+            ))}
+          </tr>
+        </thead>
       </table>
+{/* Table body */}
+      <div className="max-h-[80vh] overflow-y-auto">
+        <table className="w-full border-collapse table-fixed">
+          <tbody>
+            {data.map((row, rowIndex) => (
+              <tr key={row._id || rowIndex}>
+                {columns.map((col, colIndex) => (
+                  <td
+                    key={colIndex}
+                    className={`border border-[#F2F4F7] px-3 py-2 ${
+                      col.header === "Action"
+                        ? "bg-white hover:bg-white"
+                        : ""
+                    }`}
+                  >
+                    {col.render
+                      ? col.render(row, rowIndex)
+                      : row[col.accessor]}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
     </div>
   );
 };
