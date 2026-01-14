21bc82e1fec51fbd27aef03e434a9ebcba4eed49
taranpreet03 <taranpreet.kaur@omlogistics.co.in>
2026-01-14 14:20:13 +0530

    Hover background removed

--- src/Core/Button.jsx
+++ src/Core/Button.jsx
@@ -5,7 +5,7 @@ const Button = ({ icon: Icon, text, onClick, className = "" }) => {
 
   const themeClasses =
     theme === "dark"
-      ? "bg-[#1B211A] border border-yellow-800 text-white hover: bg-blue-800"
+      ? "bg-[#1B211A] border border-yellow-800 text-white "
       : "bg-white border border-gray-300 text-[#3A4752]";
 
   const iconClasses =
